add7f6186fb4b9ef1d089b223070eb54f2c2aaf7
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-06-28 10:43:18 +0200

    New update

--- Shared/PropretyWrapper/ScaleMetricWrapper.swift
+++ Shared/PropretyWrapper/ScaleMetricWrapper.swift
@@ -7,4 +7,40 @@
 
 import SwiftUI
 
-#warning("Refacto")
+/// Apparition d'un nouveau proprety wrapper : @ScaledMetric
+/// Disponible sur : i•Pad•OS / watchOS / tvOS / MacOS / Mac Catalyst
+///
+/// Permet de faire scale la value en fonction du dynamicType de l'utilisateur
+/// Créer des layouts responsives
+
+struct ScaleMetricWrapper: View {
+
+  @ScaledMetric private var padding: CGFloat = 10
+
+  var body: some View {
+    VStack {
+      Text("Hello ScaledMetric proprety wrapper")
+        .font(.subheadline)
+        .fontWeight(.bold)
+        .multilineTextAlignment(.center)
+      Text("A very cool new feature")
+    }
+    .padding(padding)
+    .background(Color.purple)
+    .cornerRadius(10)
+  }
+}
+
+struct ScaleMetricWrapper_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ScaleMetricWrapper()
+      ScaleMetricWrapper()
+        .environment(\.sizeCategory, .extraExtraLarge)
+      ScaleMetricWrapper()
+        .environment(\.sizeCategory, .extraSmall)
+      ScaleMetricWrapper()
+        .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+    }
+  }
+}
