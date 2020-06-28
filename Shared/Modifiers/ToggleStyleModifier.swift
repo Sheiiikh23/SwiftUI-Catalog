b5f35362769a60abc2d75f2f01e29035f8dfbb2d
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-06-28 11:45:57 +0200

    New udpate

---
+++ Shared/Modifiers/ToggleStyleModifier.swift
@@ -0,0 +1,56 @@
+//
+//  ToggleStyleModifier.swift
+//  SwiftUI 2.0
+//
+//  Created by Lucas Abijmil on 28/06/2020.
+//
+
+import SwiftUI
+
+/// Nouveau ToggleStyle modifier pour les toggle :
+/// Disponible sur i•Pad•OS / watchOS / macOS / Mac Catalyst
+///
+/// ToggleStyle présents nativement:
+///   • DefaultToggleStyle ––> par défaut, vert
+///   • SwitchToggleStyle ––> pareil que DefaultToggleStyle
+///   • SwitchToggleStyle(tint: Color) ––> custom le "foregroundColor" du toggle lorsque la value est true
+
+struct ToggleStyleModifier: View {
+
+  @State private var toggleValue = true
+
+  var body: some View {
+    VStack(spacing: 50) {
+
+      /// DefaultToggleStyle
+      Toggle(isOn: $toggleValue) {
+        Text("Look at this default toggleStyle")
+      }
+      .toggleStyle(DefaultToggleStyle())
+
+      /// SwitchToggleStyle sans tint ––> pareil que le default
+      Toggle(isOn: $toggleValue) {
+        Text("Look again at this default toggleStyle")
+      }
+      .toggleStyle(SwitchToggleStyle())
+
+      /// SwitchToggleStyle avec une tint custom
+      Toggle(isOn: $toggleValue) {
+        Text("Look at this beautiful toggleStyle")
+      }
+      .toggleStyle(SwitchToggleStyle(tint: .orange))
+
+      /// SwitchToggleStyle avec une tint custom
+      Toggle(isOn: $toggleValue) {
+        Text("Look (again) at this beautiful toggleStyle")
+      }
+      .toggleStyle(SwitchToggleStyle(tint: .pink))
+    }
+  }
+}
+
+struct ToggleStyleModifier_Previews: PreviewProvider {
+  static var previews: some View {
+    ToggleStyleModifier()
+  }
+}
