e6f17db6f6703b26ab036b192fb5347b7cecf75f
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-06-28 13:02:06 +0200

    New update

--- Shared/Views/SignInWithappleButtonView.swift
+++ Shared/Views/SignInWithappleButtonView.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
+/// Apparition du boutton sign in with Apple en natif: SignInWithAppleButton
+/// Disponible sur i•Pad•OS / tvOS / macOS / Mac Catalyst
+///
+/// Un seul constructeur prenant 3 paramètres :
+///   • Label : .continue, .signIn, .signUp ––> change le label du butotn
+///   • onRequest : some action
+///   • onCompletion: some action
+///
+/// Pour voir plus en détail les onRequest et onCompletion cf doc : https://developer.apple.com/documentation/swiftui/signinwithapplebutton
+
+
 struct SignInWithappleButtonView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    VStack(spacing: 25) {
+
+      /// Label continue
+      SignInWithAppleButton(
+        .continue,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
+
+      /// Label signIn
+      SignInWithAppleButton(
+        .signIn,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
+
+      /// Label signUp
+      SignInWithAppleButton(
+        .signUp,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
     }
+  }
 }
 
 struct SignInWithappleButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignInWithappleButtonView()
-    }
+  static var previews: some View {
+    SignInWithappleButtonView()
+  }
 }
