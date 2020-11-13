66304db72a3d2325d53a73c80796108ef3badc3e
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-13 15:05:54 +0100

    Rework AppStoreOverlay

--- Shared/Modifiers/AppStoreOverlayModifier.swift
+++ Shared/Modifiers/AppStoreOverlayModifier.swift
@@ -6,57 +6,52 @@
 //
 
 import SwiftUI
-//import StoreKit
+import StoreKit
 
 
-/// Nouveau modifier qui permet d'afficher une bannière pour recommander une app
+/// AppStoreOverlay qui permet de montrer une bannière pour recommander une app
 /// Disponible sur : i•Pad•OS
 ///
 /// Importer StoreKit
 /// Paramètres du modifier :
 ///   • isPresented: *Binding* Bool ––> obligatoire
-///   • configuration: closure ––> obigatoire (@escaping par défaut)
+///   • configuration: SKOverlay.AppConfiguration(appIdentifier: appId, position: position)
+///     - appId: String correspondant à l'ID de l'app
+///     - position : .bottom ou .bottomRaised
 
-// MARK: Utilisation : Dans App Clip permet d'inciter des gens à télécharger l'application dans son entiérreté || Recommander d'autres applications
 
+// MARK: Utilisation : Recommander d'autres applications ou dans AppClip pour montrer l'app en entière
 
-// Source : https://developer.apple.com/forums/thread/656574?answerId=625987022#625987022
 
 struct AppStoreOverlayModifier: View {
 
   @State private var showRecommandedApp = true
 
-    var body: some View {
-      ZStack {
-        Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)).edgesIgnoringSafeArea(.all)
-        Button {
-          self.showRecommandedApp.toggle()
-        } label: {
-          Text("Show recommanded app in a banner")
-            .font(.title2)
-            .fontWeight(.bold)
-            .foregroundColor(.purple)
-            .padding()
-            .background(Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)).opacity(0.5))
-            .cornerRadius(15)
-            .shadow(radius: 15)
-        }
-      }
-
-      .edgesIgnoringSafeArea(.all)
-//      .appStoreOverlay(isPresented: $showRecommandedApp) {
-//        /// Overlay qui permet d'afficher la bannière de l'app recommandée
-//        /// appIdentifier: id de l'app
-//        /// Deux positions possibles :
-//        ///   • bottom : le plus bas tout en respectant la safe area bottom (même si ignoré)
-//        ///   • bottomRaised : plus haut le bottom de genre 10-15 points
-//        SKOverlay.AppConfiguration(appIdentifier: "1440611372", position: .bottom)
-//      }
+  var body: some View {
+    Button { showRecommandedApp.toggle() } label: {
+      Text("Show recommanded app in a banner")
+        .font(.title2)
+        .fontWeight(.bold)
+        .foregroundColor(.purple)
+        .padding()
+        .background(Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)).opacity(0.5))
+        .cornerRadius(15)
+        .shadow(radius: 15)
     }
+    .appStoreOverlay(isPresented: $showRecommandedApp) {
+      /// Overlay qui permet d'afficher la bannière de l'app recommandée
+      /// appIdentifier: id de l'app
+      /// Deux positions possibles :
+      ///   • bottom : le plus bas tout en respectant la safe area bottom (même si ignoré)
+      ///   • bottomRaised : plus haut le bottom de genre 10-15 points
+      SKOverlay.AppConfiguration(appIdentifier: "1440611372", position: .bottom)
+    }
+    .ignoresSafeArea()
+  }
 }
 
 struct AppStoreOverlayModifier_Previews: PreviewProvider {
-    static var previews: some View {
-        AppStoreOverlayModifier()
-    }
+  static var previews: some View {
+    AppStoreOverlayModifier()
+  }
 }
