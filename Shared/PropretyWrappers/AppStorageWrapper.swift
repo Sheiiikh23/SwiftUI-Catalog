1e2144b6b22c6621f2a3dbce2893166d53126c24
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-13 11:10:23 +0100

    Rework AppStorage

--- Shared/PropretyWrappers/AppStorageWrapper.swift
+++ Shared/PropretyWrappers/AppStorageWrapper.swift
@@ -7,61 +7,117 @@
 
 import SwiftUI
 
-/// Apparition d'un nouveau proprety wrapper: @AppStorage qui est le "userDefault"
+/// @AppStorage qui est le wrapper pour les UserDefaults
 /// Disponible sur : i•Pad•OS / watchOS / tvOS / MacOS / Mac Catalyst
 ///
-/// Permets de lire et écrire des datas dans le standard userDefault
-/// Le wrapper permet également de créer d'autre classe dans le userDefault
-///   • ajout du paramètre store: UserDefault(suiteName: String)
+/// Permets de lire et écrire des datas dans le UserDefaults (standard ou dans un groupe)
+///
+/// Le wrapper permet également de créer d'autre groupe dans le UserDefaults
+///   • ajout du paramètre store: UserDefaults(suiteName: String)
+///
+/// Pas très solide (se baser sur des String à chaque fois peut mener à des erreurs, pareil pour le suiteName du UserDefaults)
+/// Je propose donc quelques idées plus bas en extension
+
+// MARK: - Utilisation : pour stocker en mémoire des données de petites taills non sensibles
 
 struct AppStorageWrapper: View {
 
+  // AppStorage classique
   @AppStorage("username") var username = "Anonymous"
-  @AppStorage("username", store: UserDefaults(suiteName: "group")) var usernameGroup = "Anonymous"
+  @AppStorage("username", store: UserDefaults(suiteName: "instagram")) var usernameGroup = "Anonymous"
 
-    var body: some View {
-      VStack(spacing: 50) {
-        Text("Welcome \(username) standard UserDefault")
-          .font(.title3)
+  // AppStorage with standard UserDefaults custom
+  @AppStorage(.firstName) var firstName
+  // AppStorage with group UserDefaults custom
+  @AppStorage(.lastName, store: .swiftui) var lastName
+
+  var body: some View {
+    VStack(spacing: 50) {
+      VStack(spacing: 15) {
+        Text("App Storage not custom")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.red)
+          .padding(.horizontal, 20)
+          .padding(.vertical)
+          .background(Color.green.opacity(0.5))
+          .cornerRadius(30)
+        Text("Welcome to \(username) in standard UserDefault")
           .fontWeight(.bold)
           .multilineTextAlignment(.center)
           .foregroundColor(.red)
-          .padding()
-          .background(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)))
-          .cornerRadius(15)
-          .shadow(radius: 15)
         Button {
+          // UserDefaults.standard.setValue("@lucas_abijmil", forKey: "username")
           username = "@lucas_abijmil"
-          /// Pareil que
-          ///UserDefaults.standard.setValue("@lucas_abijmil", forKey: "username")
-
         } label: {
-          Text("Log in for UserDefault standard")
+          Text("Change Anonymous for @lucas_abijmil")
         }
-        Text("Welvome \(usernameGroup) group UserDefault")
-          .font(.title3)
+        Text("Welcome to \(usernameGroup) in instagram UserDefault")
           .fontWeight(.bold)
           .multilineTextAlignment(.center)
           .foregroundColor(.red)
-          .padding()
-          .background(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)))
-          .cornerRadius(15)
-          .shadow(radius: 15)
-
         Button {
+          // UserDefaults(suiteName: "group")?.setValue("@lucasIOSDev", forKey: "usernameGroup")
           usernameGroup = "@lucasIOSDev"
-          /// Pareil que
-//          UserDefaults(suiteName: "group")?.setValue("@lucasIOSDev", forKey: "usernameGroup")
-
         } label: {
-          Text("Log in for group UserDefault")
+          Text("Change Anonymous for @lucasIOSDev")
+        }
+      }
+      Divider()
+        .padding()
+      VStack(spacing: 15) {
+        Text("App Storage custom")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.red)
+          .padding(.horizontal, 20)
+          .padding(.vertical)
+          .background(Color.green.opacity(0.5))
+          .cornerRadius(30)
+        // On aurait pu faire aussi un if let pour ne pas affiché lorsque firstName == nil
+        Text("Welcome to \(firstName ?? "Uknown") in standard UserDefault")
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.red)
+        Button { firstName = "Lucas" } label: { Text("Change Uknown for Lucas") }
+        // On aurait pu faire aussi un if let pour ne pas affiché lorsque lastName == nil
+        Text("Welcome to \(lastName ?? "Uknown") in instagram UserDefault")
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.red)
+        Button { lastName = "Abijmil" } label: { Text("Change Uknown for Abijmil") }
       }
     }
-    }
+  }
+}
+
+// MARK: - Solid AppStorage with standard UserDefaults (uniquement pour les String ici)
+extension AppStorage where Value == String? {
+  init(_ key: AppStorageKey) {
+    self.init(key.rawValue)
+  }
+}
+
+
+// MARK: - Solid AppStorage with group UserDefaults (uniquement pour les String ici)
+extension AppStorage where Value == String? {
+
+  init(_ key: AppStorageKey, store: AppStorageStore) {
+    self.init(key.rawValue, store: UserDefaults(suiteName: store.rawValue))
+  }
+}
+
+enum AppStorageKey: String {
+  case firstName
+  case lastName
+}
+
+enum AppStorageStore: String {
+  case swiftui
 }
 
 struct AppStorageWrapper_Previews: PreviewProvider {
-    static var previews: some View {
-        AppStorageWrapper()
-    }
+  static var previews: some View {
+    AppStorageWrapper()
+  }
 }
