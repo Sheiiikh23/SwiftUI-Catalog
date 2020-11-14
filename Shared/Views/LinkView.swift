f2a9d6e56b4ba96cd11779150d8849a1a89827ec
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-14 17:30:22 +0100

    Rework LinkView

--- Shared/Views/LinkView.swift
+++ Shared/Views/LinkView.swift
@@ -7,39 +7,33 @@
 
 import SwiftUI
 
-/// Apparition des Link: redirige vers un site / appli
+/// Link: redirige vers un site en respectant le nav par défaut du user / appli (si deeplink)
 /// Disponible sur : i•Pad•OS / watchOS / tvOS / macOS / Mac Catalyst
 ///
-/// Deux construteurs :
-///   • Link(String, destination: URL)
-///   • Link(destination: URL) { ... }
-///
+/// Très utile pour les widgets sur des objets où même dans des applis en tant que tell
 
-// MARK: - Utilisation : Permet d'ouvrir des URLs qui redirigent vers Safari, Universal Link pour ouvrir une App, DeepLink pour un Widget
+// MARK: - Utilisation : Permet de rediriger vers le Web / Appli (deeplink)
 
 struct LinkView: View {
-  
-  let siteApple = URL(string: "https://developer.apple.com/tutorials/swiftui/")!
-  let wwdcAnnouncement = URL(string: "https://apple.news/AjriX1CWUT-OfjXu_R4QsnA")!
-  
+
   var body: some View {
     VStack(spacing: 30) {
-      /// Constructeur "compact"
-      Link("Site d'Apple", destination: siteApple)
+
+      // Link avec une String en tant que Title
+      Link("Site d'Apple", destination: URL(string: "https://developer.apple.com/tutorials/swiftui/")!)
         .font(.title3)
         .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
         .padding()
         .background(Color.orange.opacity(0.5))
         .cornerRadius(20)
       
-      /// Constructeur permettant plus de custom
-      Link(destination: wwdcAnnouncement) {
-        Text("Voir les annonces de la WWDC 20")
-          .font(.title3)
-          .fontWeight(.bold)
-          .foregroundColor(.black)
+      /// Link avec une View en tant que label
+      /// Plus personnalisable
+      Link(destination: URL(string: "https://apple.news/AjriX1CWUT-OfjXu_R4QsnA")!) {
+        Label("Apple news", systemImage: "applelogo")
+          .foregroundColor(.red)
           .padding()
-          .background(Color.pink.opacity(0.5))
+          .background(Color.pink.opacity(0.25))
           .cornerRadius(20)
       }
     }
