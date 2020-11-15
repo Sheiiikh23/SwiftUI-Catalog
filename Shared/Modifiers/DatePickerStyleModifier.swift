3f1911281955c1aba9a58e9ab1691fff86dddbf0
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-15 13:23:09 +0100

    Rework DatePickerStyle

--- Shared/Modifiers/DatePickerStyleModifier.swift
+++ Shared/Modifiers/DatePickerStyleModifier.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-/// Nouveau DatePickerStyle pour les DatePicker
+/// DatePickerStyle pour définir le style d'un DatePicker
 /// Disponible sur : i•Pad•OS / macOS / Mac Catalyst
 ///
-///   • DefaultDatePickerStyle ––> pour les 3
-///   • WheelDatePickerStyle ––> pour i•Pad•OS / Mac Catalyst
-///   • FieldDatePickerStyle ––> macOS uniquement
-///   • GrapicalDatePickerStyle –––> pour les 3
-///   • StepperFieldDatePickerStyle ––> macOS uniquement
-/// (je n'ai pas fais de View pour ce présent uniquement sur macOS)
+///   • DefaultDatePickerStyle : style par défaut en fonction de la plateforme
+///   • CompactDatePickerStyle : pour les 3 (format textuel a utilisé lorsque l'espace est petit dans une pop-up 👍)
+///   • FieldDatePickerStyle ––> macOS uniquement (date & heure spécifique. Mais devrait utilisé le StepperStyle à moins qu'on doit caché le stepper)
+///   • GrapicalDatePickerStyle –––> pour les 3 (parcourir des mois / années 👌)
+///   • StepperFieldDatePickerStyle ––> macOS uniquement (date & heure spécifique avec un stepper pour faire varier les variables)
+///   • WheelDatePickerStyle ––> pour i•Pad•OS / Mac Catalyst (old style 🤮)
 
 // MARK: - Utilisation: Custom la UI d'un DatePicker 
 
@@ -35,6 +35,73 @@ struct DefaultDatePickerStyleModifier: View {
   }
 }
 
+struct CompactDatePickerStyleModifier: View {
+
+  @State private var date = Date()
+
+  var body: some View {
+    VStack {
+      Text("Enter your birthday")
+        .font(.largeTitle)
+      DatePicker("Enter your birthday", selection: $date)
+        .datePickerStyle(CompactDatePickerStyle())
+        .labelsHidden()
+        .frame(height: 400)
+    }
+  }
+}
+
+#if os(macOS)
+struct FieldDatePickerStyleModifier: View {
+
+  @State private var date = Date()
+
+  var body: some View {
+    VStack {
+      Text("Enter your birthday")
+        .font(.largeTitle)
+      DatePicker("Enter your birthday", selection: $date)
+        .datePickerStyle(FieldDatePickerStyle())
+        .labelsHidden()
+        .frame(height: 400)
+    }
+  }
+}
+#endif
+
+struct GraphicalDatePickerStyleModifier: View {
+
+  @State private var date = Date()
+
+  var body: some View {
+    VStack {
+      Text("Enter your birthday")
+        .font(.largeTitle)
+      DatePicker("Enter your birthday", selection: $date)
+        .datePickerStyle(GraphicalDatePickerStyle())
+        .labelsHidden()
+    }
+  }
+}
+
+#if os(macOS)
+struct StepperFieldDatePickerStyleModifier: View {
+
+  @State private var date = Date()
+
+  var body: some View {
+    VStack {
+      Text("Enter your birthday")
+        .font(.largeTitle)
+      DatePicker("Enter your birthday", selection: $date)
+        .datePickerStyle(StepperFieldDatePickerStyle())
+        .labelsHidden()
+        .frame(height: 400)
+    }
+  }
+}
+#endif
+
 struct WheelDatePickerStyleModifier: View {
 
   @State private var date = Date()
@@ -51,28 +118,13 @@ struct WheelDatePickerStyleModifier: View {
   }
 }
 
-struct GraphicalDatePickerStyleModifier: View {
-
-  @State private var date = Date()
-
-  var body: some View {
-    VStack {
-      Text("Enter your birthday")
-        .font(.largeTitle)
-      DatePicker("Enter your birthday", selection: $date)
-        .datePickerStyle(GraphicalDatePickerStyle())
-        .labelsHidden()
-        .frame(height: 400)
-    }
-  }
-}
-
 struct DefaultDatePickerStyleModifier_Previews: PreviewProvider {
   static var previews: some View {
     Group {
       DefaultDatePickerStyleModifier()
-      WheelDatePickerStyleModifier()
+      CompactDatePickerStyleModifier()
       GraphicalDatePickerStyleModifier()
+      WheelDatePickerStyleModifier()
     }
   }
 }
