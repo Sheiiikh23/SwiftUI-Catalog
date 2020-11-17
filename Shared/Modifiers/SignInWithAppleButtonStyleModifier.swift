09d6e94f9ceb5bfb983b1539b6d22cbd0a32d393
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-17 11:05:27 +0100

    Rework SignInWithappleButtonView

--- Shared/Modifiers/SignInWithAppleButtonStyleModifier.swift
+++ Shared/Modifiers/SignInWithAppleButtonStyleModifier.swift
@@ -1,70 +1,69 @@
-////
-////  SignInWithAppleButtonStyleModifier.swift
-////  SwiftUI 2.0
-////
-////  Created by Lucas Abijmil on 28/06/2020.
-////
 //
-//import SwiftUI
+//  SignInWithAppleButtonStyleModifier.swift
+//  SwiftUI 2.0
 //
-///// Modifier SignInWithAppleButtonStyle propre pour le signInWithAppleButton
-///// Disponible sur : i•Pad•OS / tvOS / macOS / Mac Catalyst
-/////
-///// Trois SignInWithAppleButtonStyle natifs :
-/////   • black : background noir, Text blanc
-/////   • white : background blanc, Text noir
-/////   • whiteOutline : background blanc, Text noir, bordure noir
+//  Created by Lucas Abijmil on 28/06/2020.
 //
-//// MARK: - Utilisation : Custom la UI de SignInWithAppleButton View
-//
-//struct SignInWithAppleButtonStyleModifier: View {
-//  var body: some View {
-//    VStack(spacing: 25) {
-//
-//      SignInWithAppleButton
-//
-//      /// Label continue
-//      SignInWithAppleButton(
-//        .continue,
-//        onRequest: {_ in }, /// Somme action here
-//        onCompletion: {_ in } /// Somme action here
-//      )
-//      .frame(width: 300, height: 200)
-//      .signInWithAppleButtonStyle(.black)
-//
-//      /// Label signIn
-//      SignInWithAppleButton(
-//        .signIn,
-//        onRequest: {_ in }, /// Somme action here
-//        onCompletion: {_ in } /// Somme action here
-//      )
-//      .frame(width: 300, height: 200)
-//      .signInWithAppleButtonStyle(.white)
-//
-//      /// Label signUp
-//      SignInWithAppleButton(
-//        .signUp,
-//        onRequest: {_ in }, /// Somme action here
-//        onCompletion: {_ in } /// Somme action here
-//      )
-//      .frame(width: 300, height: 200)
-//      .signInWithAppleButtonStyle(.whiteOutline)
-//    }
-//  }
-//}
-//
-//#warning("TODO")
-///// Essayer de créer un CustomSignInWithAppleButtonStyle
-/////
-//
-////struct CustomSignInWithAppleButtonStyle: SignInWithAppleButton.Style {
-////  func
-////}
-//
-//struct SignInWithAppleButtonStyleModifier_Previews: PreviewProvider {
-//  static var previews: some View {
-//    SignInWithAppleButtonStyleModifier()
-//  }
+
+import SwiftUI
+import AuthenticationServices
+
+/// Modifier SignInWithAppleButtonStyle propre pour le signInWithAppleButton
+/// Disponible sur : i•Pad•OS / tvOS / macOS / Mac Catalyst
+///
+/// Trois SignInWithAppleButtonStyle natifs :
+///   • black : background noir, Text blanc
+///   • white : background blanc, Text noir
+///   • whiteOutline : background blanc, Text noir, bordure noir
+
+// MARK: - Utilisation : Custom la UI de SignInWithAppleButton View
+
+struct SignInWithAppleButtonStyleModifier: View {
+  var body: some View {
+    VStack(spacing: 25) {
+
+      SignInWithAppleButton
+
+      /// Label continue
+      SignInWithAppleButton(
+        .continue,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
+      .signInWithAppleButtonStyle(.black)
+
+      /// Label signIn
+      SignInWithAppleButton(
+        .signIn,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
+      .signInWithAppleButtonStyle(.white)
+
+      /// Label signUp
+      SignInWithAppleButton(
+        .signUp,
+        onRequest: {_ in }, /// Somme action here
+        onCompletion: {_ in } /// Somme action here
+      )
+      .frame(width: 300, height: 200)
+      .signInWithAppleButtonStyle(.whiteOutline)
+    }
+  }
+}
+
+/// Essayer de créer un CustomSignInWithAppleButtonStyle
+///
+
+//struct CustomSignInWithAppleButtonStyle: SignInWithAppleButton.Style {
+//  func
 //}
 
-#warning("refacto")
+struct SignInWithAppleButtonStyleModifier_Previews: PreviewProvider {
+  static var previews: some View {
+    SignInWithAppleButtonStyleModifier()
+  }
+}
+
--- Shared/Views/SignInWithappleButtonView.swift
+++ Shared/Views/SignInWithappleButtonView.swift
@@ -1,61 +1,77 @@
-////
-////  SignInWithappleButtonView.swift
-////  SwiftUI 2.0
-////
-////  Created by Lucas Abijmil on 28/06/2020.
-////
 //
-//import SwiftUI
+//  SignInWithappleButtonView.swift
+//  SwiftUI 2.0
 //
-///// Apparition du boutton sign in with Apple en natif: SignInWithAppleButton
-///// Disponible sur i•Pad•OS / tvOS / macOS / Mac Catalyst
-/////
-///// Un seul constructeur prenant 3 paramètres :
-/////   • Label : .continue, .signIn, .signUp ––> change le label du button
-/////   • onRequest : some action
-/////   • onCompletion: some action
-/////
-///// Pour voir plus en détail les onRequest et onCompletion cf doc : https://developer.apple.com/documentation/swiftui/signinwithapplebutton
+//  Created by Lucas Abijmil on 28/06/2020.
 //
-//// MARK: - Utilisation : Button pré-build par Apple (obligatoire si on propose la connexion avec des tiers Facebook / Google)
-//
-//
-//struct SignInWithappleButtonView: View {
-//  var body: some View {
-//    VStack(spacing: 25) {
-//
-//      /// Label continue
-//      SignInWithAppleButton(
-//        .continue,
-//        onRequest: {_ in }, /// Some action here
-//        onCompletion: {_ in } /// Some action here
-//      )
-//      .frame(width: 300, height: 200)
-//
-//      /// Label signIn
-//      SignInWithAppleButton(
-//        .signIn,
-//        onRequest: {_ in }, /// Some action here
-//        onCompletion: {_ in } /// Some action here
-//      )
-//      .frame(width: 300, height: 200)
-//
-//      /// Label signUp
-//      SignInWithAppleButton(
-//        .signUp,
-//        onRequest: {_ in }, /// Some action here
-//        onCompletion: {_ in } /// Some action here
-//      )
-//      .frame(width: 300, height: 200)
-//    }
-//  }
-//}
-//
-//struct SignInWithappleButtonView_Previews: PreviewProvider {
-//  static var previews: some View {
-//    SignInWithappleButtonView()
-//  }
-//}
 
+import SwiftUI
+import AuthenticationServices
 
-#warning("refacto")
+/// Apparition du boutton sign in with Apple en natif: SignInWithAppleButton
+/// Disponible sur i•Pad•OS / watchOS / tvOS / macOS / Mac Catalyst
+///
+/// Obligé d'import AuthenticationServices
+/// Paramètres :
+///  • Label : définit le texte du bouton
+///     - .continue : "Continue with Apple"
+///     - .signIn : "Sign in with Apple"
+///     - .signUp : "Sign up with Apple"
+/// • onRequest : (ASAuthorizationAppleIDRequest) -> Void
+/// • onCompletion: (Result<ASAuthorization, Error>) -> Void
+///
+/// Pour voir plus en détail les onRequest et onCompletion cf doc : https://developer.apple.com/documentation/swiftui/signinwithapplebutton
+
+// MARK: - Utilisation : Button pré-build par Apple
+
+struct SignInWithappleButtonView: View {
+  var body: some View {
+    VStack(spacing: 25) {
+
+      /// Continue with Apple
+      SignInWithAppleButton(.continue) { request in
+        print(request)
+      } onCompletion: { result in
+        switch result {
+        case .success(let authorization):
+          print("Authorized – Some actions here : \(authorization)")
+        case .failure(let error):
+          print("Error – Some actions here : \(error.localizedDescription)")
+        }
+      }
+      .padding()
+
+      /// Sign in with Apple
+      SignInWithAppleButton(.signIn) { request in
+        print(request)
+      } onCompletion: { result in
+        switch result {
+        case .success(let authorization):
+          print("Authorized – Some actions here : \(authorization)")
+        case .failure(let error):
+          print("Error – Some actions here : \(error.localizedDescription)")
+        }
+      }
+      .padding()
+
+      /// Sign up with Apple
+      SignInWithAppleButton(.signUp) { request in
+        print(request)
+      } onCompletion: { result in
+        switch result {
+        case .success(let authorization):
+          print("Authorized – Some actions here : \(authorization)")
+        case .failure(let error):
+          print("Error – Some actions here : \(error.localizedDescription)")
+        }
+      }
+      .padding()
+    }
+  }
+}
+
+struct SignInWithappleButtonView_Previews: PreviewProvider {
+  static var previews: some View {
+    SignInWithappleButtonView()
+  }
+}
