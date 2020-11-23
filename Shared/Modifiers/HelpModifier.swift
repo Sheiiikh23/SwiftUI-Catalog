5568574c6647d59ec8c1c19a6a40e31138310549
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-24 00:02:51 +0100

    Rework HelpModifier

--- Shared/Modifiers/HelpModifier.swift
+++ Shared/Modifiers/HelpModifier.swift
@@ -10,14 +10,10 @@ import SwiftUI
 /// Apparition du modifier help afin de facilité l'accesibilité de l'application
 /// Disponible sur : i•Pad•OS / watcOS / tvOS / macOS / Mac Catalyst
 ///
-///   • i•Pad•OS : voiceOver lira la String contenu dans le modifier
-///   • tvOS ??
+///   • i•Pad•OS / watchOs (?) / tvOS (?) : voiceOver lira la String contenu dans le modifier
 ///   • macOS / Mac Catalyst: info bulle "classique"
 
-// MARK: - Utilisation : si on souhaite rendre notre application plus accessible (ici pour les malvoyants)
-#warning("Normalement dispo pour watchOS. Regarder les updates de la doc")
-#warning("Lever les incertitudes")
-#warning("cf accessibilityEnabled")
+// MARK: - Utilisation : Si on souhaite rendre notre application plus accessible (ici pour les malvoyants)
 
 struct HelpModifier: View {
   var body: some View {
