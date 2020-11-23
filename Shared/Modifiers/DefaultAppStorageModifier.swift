0a50b6d1652eed41ce1e2ea4363a180cf3ba70b5
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-23 19:40:37 +0100

    Rework DefaultAppStorage

--- Shared/Modifiers/DefaultAppStorageModifier.swift
+++ Shared/Modifiers/DefaultAppStorageModifier.swift
@@ -7,5 +7,36 @@
 
 import SwiftUI
 
-#warning("Refacto")
-//.defaultAppStorage(.standard)
+/// DefaultAppStorage permet de spécifier le store par défaut des @AppStorage de la vue ou d'un container
+/// Disponible sur : i•Pad•OS / watchOS / tvOS / MacOS / Mac Catalyst
+///
+/// À utiliser avec des @AppStorage mais peu d'utilité vu qu'on peut le set lors de la déclaration de cette variable
+
+// MARK: - Utilisation : Avec des @AppStorage mais utilisation assez minime selon moi
+
+struct DefaultAppStorageModifier: View {
+
+  @AppStorage(.name) var name
+
+  var body: some View {
+    VStack {
+      VStack {
+        TextField("Enter your name", text: $name)
+          .keyboardType(.default)
+      }
+      .defaultAppStorage(UserDefaults.init(suiteName: "user")!)
+    }
+  }
+}
+
+fileprivate extension AppStorage where Value == String {
+  init(_ key: AppStorageKeys) {
+    self.init(wrappedValue: "", key.rawValue)
+  }
+}
+
+fileprivate enum AppStorageKeys: String {
+  case name
+}
+
+
