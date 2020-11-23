5e453d483ba6072b4d6d2646e5b911bcd2df704f
Lucas Abijmil <lucas.abijmil@gmail.com>
2020-11-24 00:29:20 +0100

    Rework WindowStyleModifier

--- Shared/Modifiers/WindowStyleModifier.swift
+++ Shared/Modifiers/WindowStyleModifier.swift
@@ -7,4 +7,30 @@
 
 import SwiftUI
 
-#warning("Refacto")
+/// WindowStyle permet de spécifier l'apparencet et l'interaction d'une fenêtre
+/// Disponible sur : macOS
+///
+/// Trois style natifs :
+///   • DefaultWindowStyle : par défaut
+///   • HiddenTitleBarWindowStyle : cache le titre et son l'arrière bar de la fenêtre, permet de montrer une plus grande partie du contenu de la fenêtre
+///   • TitleBarWindowStyle : affiche la section de la barre de titre de la fenêtre
+
+#if os(macOS)
+struct WindowStyleModifier: App {
+
+  @State private var favStyle: FavWindowStyle = .default
+
+  var body: some Scene {
+    WindowGroup {
+      LabelView()
+    }
+    .windowStyle(favStyle == .default ? DefaultWindowStyle() : favStyle == .hiddenTitleBar ? HiddenTitleBarWindowStyle() : TitleBarWindowStyle())
+  }
+}
+#endif
+
+fileprivate enum FavWindowStyle {
+  case `default`
+  case hiddenTitleBar
+  case titleBar
+}
